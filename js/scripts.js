35b6c71a6228e71d57231e3a8d2b0a20ac040075
George <loaizageorge@gmail.com>
2016-07-09 16:56:36 -0400

    Figure out REAL way to call proper format(Search+suggested articles)

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 $(document).ready(getWiki);
 
 function getWiki(){
-    var URL = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=apple&srinfo=suggestion&callback=?";
+    var URL = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=apple&prop=info&inprop=url&callback=?";
     
    $.getJSON(URL,function(json){
-      console.log(JSON.stringify(json));
-       //console.log(json.search.title);
+      $("#title").html(json.query.search[2].title);
+       $("#snippet").html(json.query.search[2].snippet);
+       console.log(json.query.search[0].title);
    });
 }
