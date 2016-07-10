336dac819e31edc8cdc4ab9e15691e61d40f35f9
George <loaizageorge@gmail.com>
2016-07-10 17:31:26 -0400

    Click on search hides main page and shows results(no links)

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,31 @@
-$(document).ready(getWiki);
+$(document).ready(function(){
+$("#search").click(function(){
+  $("#test").hide();
+});
+    });
 
-function getWiki(){
-    var URL = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=apple&prop=info&inprop=url&callback=?";
+
+function getWiki(keyword){
+    var URL = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch="+keyword+"&prop=info&inprop=url&callback=?";
     
    $.getJSON(URL,function(json){
-      $("#title").html(json.query.search[2].title);
-       $("#snippet").html(json.query.search[2].snippet);
-       console.log(json.query.search[0].title);
+       $(".hidden-heading").show();
+      $("#test-title-1").html(json.query.search[0].title);
+       $("#test-snippet-1").html(json.query.search[0].snippet);
+        $("#test-title-2").html(json.query.search[1].title);
+       $("#test-snippet-2").html(json.query.search[1].snippet);
+        $("#test-title-3").html(json.query.search[2].title);
+       $("#test-snippet-3").html(json.query.search[2].snippet);
+        $("#test-title-4").html(json.query.search[3].title);
+       $("#test-snippet-4").html(json.query.search[3].snippet);
+        $("#test-title-5").html(json.query.search[4].title);
+       $("#test-snippet-5").html(json.query.search[4].snippet);
+      
    });
+}
+
+function sendToPage(){
+    var input = document.getElementById("keyword").value;
+    getWiki(input);
+    
 }
